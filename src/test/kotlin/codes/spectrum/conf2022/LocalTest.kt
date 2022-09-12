4264986b4ec3e3838d988fb7e92950017cb8f581
lokbugs <lokbugs@gmail.com>
2022-09-13 03:40:26 +0500

    Add step to run LocalTest.kt

--- src/test/kotlin/codes/spectrum/conf2022/LocalTest.kt
+++ src/test/kotlin/codes/spectrum/conf2022/LocalTest.kt
@@ -1,25 +1,17 @@
 package codes.spectrum.conf2022
 
 import codes.spectrum.conf2022.engine.TestDesc
-import codes.spectrum.conf2022.input.IDocParser
 import codes.spectrum.conf2022.input.RandomSuccessfulParser
 import codes.spectrum.conf2022.output.ExpectedResult
-import codes.spectrum.conf2022.output.ExtractedDocument
-import io.kotest.core.spec.Spec
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.core.test.TestCase
-import io.kotest.core.test.TestCaseOrder
 import io.kotest.inspectors.forAll
-import io.kotest.matchers.booleans.shouldBeFalse
-import io.kotest.matchers.booleans.shouldBeTrue
-import io.kotest.matchers.shouldBe
 import kotlinx.coroutines.launch
 import java.io.File
 
 class LocalTest : FunSpec() {
-    val inputFileName = "local.csv"
-    val testDescFile = File(inputFileName)
-    val docParser = RandomSuccessfulParser()
+    private val inputFileName = "local.csv"
+    private val testDescFile = File(inputFileName)
+    private val docParser = RandomSuccessfulParser()
 
     init {
         context("локальные тесты") {
