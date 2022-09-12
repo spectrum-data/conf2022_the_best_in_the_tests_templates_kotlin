dae60fa1b1123d9248f7ff36a4614d73d4bc3eb2
Харисов Данил <lokbugs@gmail.com>
2022-09-12 13:03:03 +0500

    add examples of tests. add okato and regions dictionaries.

--- src/main/kotlin/codes/spectrum/conf2022/input/IDocParser.kt
+++ src/main/kotlin/codes/spectrum/conf2022/input/IDocParser.kt
@@ -1,8 +1,26 @@
 package codes.spectrum.conf2022.input
 
 import codes.spectrum.conf2022.doc_type.DocType
+import codes.spectrum.conf2022.output.ExpectedResult
 import codes.spectrum.conf2022.output.ExtractedDocument
+import kotlin.random.Random
 
 interface IDocParser {
     fun parse(input: String): List<ExtractedDocument>
+}
+
+class RandomSuccessfulParser : IDocParser {
+    override fun parse(input: String): List<ExtractedDocument> {
+        return if (Random.nextBoolean()) {
+            parseByExpectedResult(input)
+        } else {
+            emptyList()
+        }
+    }
+
+    private fun parseByExpectedResult(input: String): List<ExtractedDocument> {
+        val expectedResult = ExpectedResult.parse(input)
+
+        return expectedResult.expected
+    }
 }
--- src/test/kotlin/codes/spectrum/conf2022/LocalTest.kt
+++ src/test/kotlin/codes/spectrum/conf2022/LocalTest.kt
@@ -2,6 +2,7 @@ package codes.spectrum.conf2022
 
 import codes.spectrum.conf2022.engine.TestDesc
 import codes.spectrum.conf2022.input.IDocParser
+import codes.spectrum.conf2022.input.RandomSuccessfulParser
 import codes.spectrum.conf2022.output.ExpectedResult
 import codes.spectrum.conf2022.output.ExtractedDocument
 import io.kotest.core.spec.Spec
@@ -18,12 +19,13 @@ import java.io.File
 class LocalTest : FunSpec() {
     val inputFileName = "local.csv"
     val testDescFile = File(inputFileName)
+    val docParser = RandomSuccessfulParser()
 
     init {
         context("локальные тесты") {
             val testDescValidateResult = TestDesc.validate(testDescFile)
 
-            test("валидация входного файла"){
+            test("валидация входного файла") {
                 assert(testDescValidateResult.isValid) {
                     "Входной файл ${inputFileName} не валиден:\n" +
                             testDescValidateResult.errorMessages.joinToString("\n")
@@ -32,11 +34,18 @@ class LocalTest : FunSpec() {
 
             if (testDescValidateResult.isValid) {
                 TestDesc.parseFromFile(testDescFile).forAll { testDesc ->
+
+                    testDesc.isDisabled
+
                     launch {
                         test("${testDesc.author} №${testDesc.number}") {
                             val expectedResult = ExpectedResult.parse(testDesc.stringToProcessed)
 
-                            assert(expectedResult.match(emptyList())) { "${testDesc.commentOnFailure}. Входная строка: ${testDesc.stringToProcessed}" }
+                            val actualResult = docParser.parse(testDesc.stringToProcessed)
+
+                            assert(expectedResult.match(actualResult)) {
+                                "${testDesc.commentOnFailure}.\nВходная строка: ${testDesc.stringToProcessed}\nРезультат:${actualResult}"
+                            }
 
                         }
                     }
