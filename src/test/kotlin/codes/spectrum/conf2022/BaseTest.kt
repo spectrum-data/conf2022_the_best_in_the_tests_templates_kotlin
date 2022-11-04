df6cb6a258ac173ba15a6b1c2a2c2752a7d94794
harisov.d <harisov.d@avtocod.ru>
2022-11-04 19:00:47 +0600

    Запуски базовых и локальных тестов.

--- src/test/kotlin/codes/spectrum/conf2022/BaseTest.kt
+++ src/test/kotlin/codes/spectrum/conf2022/BaseTest.kt
@@ -1,26 +1,13 @@
 package codes.spectrum.conf2022
 
-import codes.spectrum.conf2022.input.IDocParser
-import codes.spectrum.conf2022.output.ExtractedDocument
-import io.kotest.core.spec.style.FunSpec
+import java.io.File
 
 /**
- * Набор базовых тестов, которым должен удовлетворять парсер документов
- *
- * Базовые тесты - умение парсить русифицированное название документа
+ * Запуск базовых тестов
  * */
-class BaseTest : FunSpec() {
-
-    /**
-     * Экземпляр парсера, который необходимо реализовать участникам
-     * */
-    val docParser = object : IDocParser {
-        override fun parse(input: String): List<ExtractedDocument> {
-            return emptyList()
-        }
-    }
-
-    init {
-
-    }
+class BaseTest : TestBase(
+    filesToProcess = listOf(
+        File(PROJECT_ROOT_DIR, BASE_TEST_FILE_NAME)
+    )
+) {
 }
