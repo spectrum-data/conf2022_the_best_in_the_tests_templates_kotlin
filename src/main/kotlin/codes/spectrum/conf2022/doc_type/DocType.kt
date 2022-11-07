62b27783da44c2657d0e373436abe736e8a60145
harisov.d <harisov.d@avtocod.ru>
2022-11-07 21:39:19 +0600

    Добавил получение регулярку для валидации нормализованного значения

--- src/main/kotlin/codes/spectrum/conf2022/doc_type/DocType.kt
+++ src/main/kotlin/codes/spectrum/conf2022/doc_type/DocType.kt
@@ -1,15 +1,74 @@
 package codes.spectrum.conf2022.doc_type
 
+/**
+ *
+ * */
 enum class DocType {
     UNDEFINED,
+
+    /**
+     * Паспорт РФ
+     * */
     PASSPORT_RF,
+
+    /**
+     * Водительское удостоверение
+     * */
     DRIVER_LICENSE,
+
+    /**
+     * Идентификационный номер транспортного средства
+     * */
     VIN,
+
+    /**
+     * Государственный регистрационный номер транспортного средства
+     * */
+    GRZ,
+
+    /**
+     * Свидетельство о регистрации транспортного средства
+     * */
     STS,
-    PTS,
+
+    /**
+     * ИНН Юр. лица
+     * */
     INN_FL,
+
+    /**
+     * ИНН Физ. лица
+     * */
     INN_UL,
-    EGRN,
-    EGRIP,
+
+    /**
+     * ОГРН
+     * */
+    OGRN,
+
+    /**
+     * ОГРНИП
+     * */
+    OGRNIP,
+
+    /**
+     * СНИЛС
+     * */
     SNILS,
+}
+
+fun getNormaliseValueRegex(docType: DocType): Regex {
+    return when (docType) {
+        DocType.PASSPORT_RF -> "\\d{10}"
+        DocType.DRIVER_LICENSE -> "\\d{10}"
+        DocType.VIN -> "[A-Z0-9]{17}"
+        DocType.GRZ -> "[АВЕКМНОРСТУХ]\\d{3}[АВЕКМНОРСТУХ]{2}\\d{2,3}"
+        DocType.STS -> "\\d{2}[А-ЯA-Z0-9]{2}\\d{6}"
+        DocType.INN_FL -> "\\d{12}"
+        DocType.INN_UL -> "\\d{10}"
+        DocType.OGRN -> "\\d{13}"
+        DocType.OGRNIP -> "\\d{15}"
+        DocType.SNILS -> "\\d{3}-\\d{3}-\\d{3}-\\d{2}"
+        DocType.UNDEFINED -> error("Попытка получить регулярное выражение для нормализации неопознанного документа - $docType")
+    }.toRegex()
 }
---
+++ src/test/kotlin/codes/spectrum/conf2022/doc_type/DocTypeTest.kt
@@ -0,0 +1,368 @@
+package codes.spectrum.conf2022.doc_type
+
+import codes.spectrum.conf2022.output.ExtractedDocument
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.scopes.FunSpecContainerContext
+import io.kotest.matchers.shouldBe
+
+class DocTypeTest : FunSpec() {
+    suspend fun FunSpecContainerContext.testNormaliseRegex(testName: String, doc: ExtractedDocument, isMatch: Boolean) {
+        test(testName) {
+            getNormaliseValueRegex(doc.docType).matches(doc.value) shouldBe isMatch
+        }
+    }
+
+    init {
+        context("Паспорт РФ") {
+            val passportRf = ExtractedDocument(docType = DocType.PASSPORT_RF)
+
+            testNormaliseRegex(
+                testName = "десять цифр без пробела - валиден",
+                doc = passportRf.copy(value = "0123456789"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "десять цифр, содержит пробел - не валиден",
+                doc = passportRf.copy(value = "0123 456789"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "девять цифр, не содержит пробел - не валиден",
+                doc = passportRf.copy(value = "123456789"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "одиннадцать цифр, не содержит пробел - не валиден",
+                doc = passportRf.copy(value = "01234567891"),
+                isMatch = false
+            )
+        }
+
+        context("Водительское удостоверение") {
+            val dl = ExtractedDocument(docType = DocType.DRIVER_LICENSE)
+
+            testNormaliseRegex(
+                testName = "десять цифр без пробела - валиден",
+                doc = dl.copy(value = "0123456789"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "десять цифр, содержит пробел - не валиден",
+                doc = dl.copy(value = "0123 456789"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "девять цифр, не содержит пробел - не валиден",
+                doc = dl.copy(value = "123456789"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "одиннадцать цифр, не содержит пробел - не валиден",
+                doc = dl.copy(value = "01234567891"),
+                isMatch = false
+            )
+        }
+
+        context("Идентификационный номер транспортного средства") {
+            val vin = ExtractedDocument(docType = DocType.VIN)
+
+            testNormaliseRegex(
+                testName = "семнадцать цифр без пробела - валиден",
+                doc = vin.copy(value = "12345678901234567"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "семнадцать заглавных латинских букв без пробела - валиден",
+                doc = vin.copy(value = "ABCDEFGHIJKLMNOPQ".also { it.length shouldBe 17 }),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "семнадцать строчных латинских букв без пробела - не валиден",
+                doc = vin.copy(value = "abcdefghijklmnopq".also { it.length shouldBe 17 }),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "латинские заглавные буквы + цифры - всего 17 - без пробела - валиден",
+                doc = vin.copy(value = "ABCDEFGHIJKLM1234".also { it.length shouldBe 17 }),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "латинские заглавные буквы + цифры - всего 17 содержат пробел - не валиден",
+                doc = vin.copy(value = "ABCDEFGHIJKLM 1234"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "шестнадцать цифр без пробела - не валиден",
+                doc = vin.copy(value = "1234567890123456"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "шестнадцать цифр без пробела - не валиден",
+                doc = vin.copy(value = "1234567890123456"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "восемнадцать цифр без пробела - не валиден",
+                doc = vin.copy(value = "123456789012345678"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "шестнадцать заглавных латинских букв без пробела - не валиден",
+                doc = vin.copy(value = "ABCDEFGHIJKLMNOP".also { it.length shouldBe 16 }),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "восемнадцать заглавных латинских букв без пробела - не валиден",
+                doc = vin.copy(value = "ABCDEFGHIJKLMNOPQR".also { it.length shouldBe 18 }),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "семнадцать заглавных букв кириллица без пробела - не валиден",
+                doc = vin.copy(value = "АБВГДЕЁЖЗИЙКЛМНОП".also { it.length shouldBe 17 }),
+                isMatch = false
+            )
+        }
+
+        context("Государственный регистрационный номер транспортного средства") {
+            val grz = ExtractedDocument(docType = DocType.GRZ)
+
+            testNormaliseRegex(
+                testName = "С227НА69 - валиден",
+                doc = grz.copy(value = "С227НА69"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "C227HA69 - написан латинскими буквами - не валиден",
+                doc = grz.copy(value = "C227HA69"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "С227НА691 - регион из 3 цифр - валиден",
+                doc = grz.copy(value = "С227НА691"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "С227НА6 - не заканчивается на две цифры - не валиден",
+                doc = grz.copy(value = "С227НА6"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "С227Н69 - не содержит перед двумя цифрами две буквы - не валиден",
+                doc = grz.copy(value = "С227Н69"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "НА69 - содержит только две цифры и две буквы - не валиден",
+                doc = grz.copy(value = "С227НА6"),
+                isMatch = false
+            )
+        }
+
+        context("Свидетельство о регистрации транспортного средства") {
+            val sts = ExtractedDocument(docType = DocType.STS)
+
+            testNormaliseRegex(
+                testName = "1234567890 - десять цифр - валиден",
+                doc = sts.copy(value = "1234567890"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "12AA567890 - две цифры две буквы заглавные шесть цифр - валиден",
+                doc = sts.copy(value = "12AA567890"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "12AA567890 - две цифры две буквы заглавные шесть цифр содержит пробел - не валиден",
+                doc = sts.copy(value = "12 AA 567890"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "12aa567890 - две цифры две буквы строчные шесть цифр - не валиден",
+                doc = sts.copy(value = "12aa567890"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "12AA56789 - две цифры две буквы строчные 5 цифр - не валиден",
+                doc = sts.copy(value = "12AA56789"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "123456789 - девять цифр - не валиден",
+                doc = sts.copy(value = "123456789"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "12345678901 - одиннадцать цифр - не валиден",
+                doc = sts.copy(value = "12345678901"),
+                isMatch = false
+            )
+        }
+
+        context("ИНН физ.лица") {
+            val innFl = ExtractedDocument(docType = DocType.INN_FL)
+
+            testNormaliseRegex(
+                testName = "двенадцать цифр без пробела - валиден",
+                doc = innFl.copy(value = "123456789012"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "двенадцать цифр, содержит пробел - не валиден",
+                doc = innFl.copy(value = "1234 56789012"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "одиннадцать цифр, не содержит пробел - не валиден",
+                doc = innFl.copy(value = "12345678901"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "тринадцать цифр, не содержит пробел - не валиден",
+                doc = innFl.copy(value = "1234567890123"),
+                isMatch = false
+            )
+        }
+
+        context("ИНН юр.лица") {
+            val innUl = ExtractedDocument(docType = DocType.INN_UL)
+
+            testNormaliseRegex(
+                testName = "десять цифр без пробела - валиден",
+                doc = innUl.copy(value = "0123456789"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "десять цифр, содержит пробел - не валиден",
+                doc = innUl.copy(value = "0123 456789"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "девять цифр, не содержит пробел - не валиден",
+                doc = innUl.copy(value = "123456789"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "одиннадцать цифр, не содержит пробел - не валиден",
+                doc = innUl.copy(value = "01234567891"),
+                isMatch = false
+            )
+        }
+
+        context("ОГРН") {
+            val ogrn = ExtractedDocument(docType = DocType.OGRN)
+
+            testNormaliseRegex(
+                testName = "тринадцать цифр без пробела - валиден",
+                doc = ogrn.copy(value = "1234567890123"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "тринадцать цифр, содержит пробел - не валиден",
+                doc = ogrn.copy(value = "1234 567890123"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "двенадцать цифр, не содержит пробел - не валиден",
+                doc = ogrn.copy(value = "123456789012"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "четырнадцать цифр, не содержит пробел - не валиден",
+                doc = ogrn.copy(value = "12345678901234"),
+                isMatch = false
+            )
+        }
+
+        context("ОГРНИП") {
+            val ogrnip = ExtractedDocument(docType = DocType.OGRNIP)
+
+            testNormaliseRegex(
+                testName = "пятнадцать цифр без пробела - валиден",
+                doc = ogrnip.copy(value = "123456789012345"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "пятнадцать цифр, содержит пробел - не валиден",
+                doc = ogrnip.copy(value = "12 3456789012345"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "шестнадцать цифр, не содержит пробел - не валиден",
+                doc = ogrnip.copy(value = "3456789012345"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "четырнадцать цифр, не содержит пробел - не валиден",
+                doc = ogrnip.copy(value = "12345678901234"),
+                isMatch = false
+            )
+        }
+        context("СНИЛС") {
+            val snils = ExtractedDocument(docType = DocType.SNILS)
+
+            testNormaliseRegex(
+                testName = "Всё через тире - валиден",
+                doc = snils.copy(value = "123-456-789-00"),
+                isMatch = true
+            )
+
+            testNormaliseRegex(
+                testName = "Последние две цифры через пробел - не валиден",
+                doc = snils.copy(value = "123-456-789 00"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "Все через тире - но содержит не одиннадцать цифр - не валиден",
+                doc = snils.copy(value = "123-46-789-00"),
+                isMatch = false
+            )
+
+            testNormaliseRegex(
+                testName = "Без тире - не валиден",
+                doc = snils.copy(value = "1234678900"),
+                isMatch = false
+            )
+        }
+
+    }
+}
